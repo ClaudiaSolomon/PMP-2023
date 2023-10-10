9bbf32d86cbd8e0ee1dc6c174428409a3dc6031a
ClaudiaSolomon <claudia.solomon39@yahoo.com>
2023-10-10 11:23:41 +0300

    +

---
+++ Tema lab 2/lab2.py
@@ -0,0 +1,21 @@
+import numpy as np
+from scipy import stats
+import random
+import matplotlib.pyplot as plt
+import arviz as az
+
+np.random.seed(1)
+m1=stats.expon(0,1/4).rvs(10000)
+m2=stats.expon(0,1/6).rvs(10000)
+
+p1=0.4
+p2=0.6
+x=p1*m1+p2*m2
+
+mean = np.mean(x)
+deviation=np.std(x)
+print(f"Medie:{mean}")
+print(f"Deviatie:{deviation}")
+
+az.plot_posterior({'x':x})
+plt.show()
