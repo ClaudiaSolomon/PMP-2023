9141dd4a7f0f8555e4770476da6ced2e12213ef7
ClaudiaSolomon <claudia.solomon39@yahoo.com>
2023-11-07 11:18:08 +0200

    +2.

--- Tema lab 6/lab6.py
+++ Tema lab 6/lab6.py
@@ -1,6 +1,7 @@
 import pymc3 as pm
 import arviz as az 
 
+#1
 Y = [0, 5, 10]
 Theta = [0.2, 0.5]
 if __name__ == '__main__':
@@ -11,3 +12,8 @@ if __name__ == '__main__':
                 nr_clienti = pm.Binomial(f'Nr clienti pentru={y} si theta={theta}', n=n, p=theta, observed=y)
         trace = pm.sample(2000, tune=1000, chains=2)
         az.plot_posterior(trace)
+
+
+#2
+- cu cat Y creste cu atat distributia lui n se modifica deoarece ea reprezinta frecventa cumpararilor a unui anumit nr de clienti
+- theta afecteaza estimarea nr total de clienti
