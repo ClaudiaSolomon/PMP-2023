6552b3c34c18116cfcd5e0016b80c207cf751288
ClaudiaSolomon <claudia.solomon39@yahoo.com>
2023-11-24 11:36:56 +0200

    +

--- Partial/Pex2.py
+++ Partial/Pex2.py
@@ -8,22 +8,34 @@ from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
 import networkx as nx
 import pymc as pm
+import copy
 #1
 mu, sigma = 0, 0.1 # valori alese de mine
-timp_asteptare = np.random.normal(mu, sigma, 100) #generare 100 de timpi 
-print(timp_asteptare)
-print(timp_asteptare.mean())
-
-#3
-
-# with pm.Model() as model:
-#     sigma = pm.Beta('sigma', alpha=1., beta=1.)
-#     y = pm.Bernoulli('y', p=sigma)
-#     timp_asteptare = pm.Normal("timp_asteptare", mu=y, sigma=sigma)
-
-#     with model:
-#         trace = pm.sample(100, cores=1)
-#         az.plot_posterior(trace)
-#         plt.show()
+meanlist = []
+for i in range(100):
+    timp_asteptare = np.random.normal(mu, sigma,10).mean()
+    meanlist.append(copy.deepcopy(timp_asteptare))
+print(meanlist)
 
 #2
+#distributia beta este conjugata a posteriori a distributiei bernoulli
+with pm.Model() as model:
+    sigma = pm.Beta('sigma', alpha=1., beta=1.)
+    y = pm.Bernoulli('y', p=sigma)
+    timp_asteptare = pm.Normal("timp_asteptare", mu=y, sigma=sigma)
+    observation = pm.Poisson("obs", mu=timp_asteptare, observed=meanlist)
+
+
+    with model:
+        trace = pm.sample(1000, cores=1)
+        az.plot_posterior(trace)
+        plt.show()
+
+#3
+with pm.Model() as model:
+        sigma = pm.Beta('sigma', alpha=1., beta=1.)
+        y = pm.Bernoulli('y', p=sigma)
+        timp_asteptare = pm.Normal("timp_asteptare", mu=y, sigma=sigma)
+        #estimare y
+        trace = pm.sample(2000, tune=1000, chains=2)
+        az.plot_posterior(trace)
