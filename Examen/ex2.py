2e4f76fc416209ebe96c615e417fe7cd9cddea3d
ClaudiaSolomon <claudia.solomon39@yahoo.com>
2024-01-29 11:07:57 +0200

    ex 2

---
+++ Examen/ex2.py
@@ -0,0 +1,28 @@
+import pymc as pm
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+#a
+def posterior_grid(grid_points=50, heads_obtinut_la_aruncarea=20):
+    grid = np.linspace(0, 1, grid_points)
+    prior = np.repeat(1/grid_points, grid_points)
+
+    likelihood = stats.geom.pmf(5*heads_obtinut_la_aruncarea, grid)
+    posterior = likelihood * prior
+    posterior /= posterior.sum()
+    return grid, posterior
+
+points = 10
+grid, posterior = posterior_grid(points, 20)
+
+plt.plot(grid, posterior, 'o-')
+plt.yticks([])
+plt.xlabel('θ')
+plt.show()
+
+#b
+MAP_estimate = grid[np.argmax(posterior)]
+print("Prob a posteriori maxima pentru theta: ", MAP_estimate)
+
+
